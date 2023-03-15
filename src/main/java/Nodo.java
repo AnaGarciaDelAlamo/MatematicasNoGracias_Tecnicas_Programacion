ad42d8a047dde4497e75b99a270a55702555e7d5
ana <andelamo@myuax.com>
2023-03-15 10:09:53 +0100

    Clase nodo

--- src/main/java/Nodo.java
+++ src/main/java/Nodo.java
@@ -24,25 +24,35 @@ while(aux is not None):
 import java.util.Scanner;
 
 public class Nodo {
+    String info;
+    Nodo sig;
 
+    public Nodo(String info){
+        this.info = info;
+        this.sig = null;
+    }
 
-
+    public Nodo(String valor, Nodo sig){
+        this.info = valor;
+        this.sig = sig;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Nodo aux = new Nodo();
-        aux.info = "Primer nodo";
-        String palabra = sc.nextLine();
+        Nodo aux = new Nodo("Primer nodo");
         Nodo naux = aux;
-        while (!palabra.equals("")) {
-            Nodo nodo = new Nodo();
-            nodo.info = palabra;
+        String palabra = sc.nextLine();
+        while(!palabra.isEmpty()){
+            Nodo nodo = new Nodo(palabra);
             naux.sig = nodo;
             naux = nodo;
             palabra = sc.nextLine();
         }
-        while (aux != null) {
+        while(aux != null){
             System.out.println(aux.info);
             aux = aux.sig;
         }
+
+
+
     }
 }
