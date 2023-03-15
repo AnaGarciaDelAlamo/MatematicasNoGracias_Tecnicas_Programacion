b02f68a6b3c605a94868b15669e567bf22505706
ana <andelamo@myuax.com>
2023-03-15 10:57:42 +0100

    Clase Polinomio con fallos y faltan metodos

--- src/main/java/Polinomio.java
+++ src/main/java/Polinomio.java
@@ -1,101 +1,87 @@
-/*traduce este codigo de python a java:
-class Polinomio(object):
-"""Clase polinomio"""
-
-def_init_(self):
-    """Crea un polinomio de grado cero"""
-    self.termino_mayor = None
-    self.grado = -1
-
-def agregar_termino(polinomio, termino, valor):
-"""Agrega un termino y su valor al polinomio"""
-aux = Nodo()
-dato = DatoPolinomio(valor, termino)
-aux.info = dato
-if(termino > polinomio.grado):
-    aux.sig = polinomio.termino_mayor
-    polinomio.termino_mayor = aux
-    polinomio.grado = termino
-else:
-    actual = polinomio.termino_mayor
-    while(actual.sig in not None and termino < actual.sig.info.termino)
-       actual = actual.sig
-    aux.sig = actual.sig
-    actual.sig = aux
-
-def modificar_termino(polinomio, termino, valor):
-"""Modificar un termino del polinomio"""
-aux = polinomio.termino_mayor
-while(aux is not None and aux.info.termino != termino):
-    aux = aux.sig
-  aux.info.valor = valor
-
-  def obtener_valor(polinomio, termino):
-  """Devuelve el valor de un termino del polinomio"""
-  aux = polinomio.termino_mayor
-  while(aux is not None and aux.info.termino > termino):
-      aux = aux.sig
-  if(aux is not None and aux.info.termino == termino):
-        return aux.info.valor
-    else:
-        return 0
-
-
-def mostrar(polinomio):
-"""Muestra el polinomio"""
-aux = polinomio.termino_mayor
-pol = ''
-if(aux is not None):
-    while(aux is not None):
-        signo = ''
-        if(aux.info.valor >= 0):
-            signo += '+'
-        pol += signo + str(aux.info.valor) + 'x^' + str(aux.info.termino)
-        aux = aux.sig
-return pol
-
-def sumar(polinomio1, polinomio2):
-"""Suma dos polinomios y devuelve el resultado"""
-paux = Polinomio()
-mayor = polinomio1 if(polinomio1.grado > polinomio2.grado) else polinomio2
-for i in range(0, mayor.grado+1):
-    total = obtener_valor(polinomio1, i) + obtener_valor(polinomio2, i)
-    if(total != 0):
-          agregar_termino(paux, i, total)
-return paux
-
-def multiplicar(polinomio1, polinomio2):
-"""Multiplica dos polinomios y devuelve el resultado"""
-paux = Polinomio()
-pol1 = polinomio1.termino_mayor
-while(pol1 is not None):
-    pol2 = polinomio2.termino_mayor
-    while(pol2 is not None):
-        termino = pol.info.termino + pol2.info.termino
-        valor = pol.info.valor * pol2.info.valor
-        if(obtener_valor(paux, termino) != 0):
-          valor += obtener_valor(paux, termino, valor)
-        else:
-          agregar_termino(paux, termino, valor)
-        pol2 = pol2.sig
-    pol1 = pol1.sig
-return paux
-
-
- */
-
 import java.util.Scanner;
 
 public class Polinomio {
-    
+    private Nodo termino_mayor;
+    private int grado;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Polinomio polinomio = new Polinomio();
-        System.out.println("Ingrese el valor del polinomio");
-        polinomio.valor = sc.nextInt();
-        System.out.println("Ingrese el termino del polinomio");
-        polinomio.termino = sc.nextInt();
+    public Polinomio(){
+        this.termino_mayor = null;
+        this.grado = -1;
+    }
+
+    public void agregarTermino(int termino, double valor){
+        Nodo aux = new Nodo();
+        DatoPolinomio dato = new DatoPolinomio(valor, termino);
+        aux.selfInfo(dato);
+        if(termino > grado){
+            aux.selfSig(termino_mayor);
+            termino_mayor = aux;
+            grado = termino;
+        } else {
+            Nodo actual = termino_mayor;
+            while(actual.getSig() != null && termino < actual.getSig().getInfo().getTermino()){
+                actual = actual.getSig();
+            }
+            aux.setSig(actual.getSig());
+            actual.setSig(aux);
+        }
+    }
+
+    public void modificarTermino(int termino, double valor){
+        Nodo aux = termino_mayor;
+        while(aux != null && aux.getInfo().getTermino() != termino){
+            aux = aux.getSig();
+        }
+        if(aux != null){
+            aux.getInfo().setValor(valor);
+        }
+    }
+
+    public double obtenerValor(int termino){
+        Nodo aux = termino_mayor;
+        while(aux != null && aux.getInfo().getTermino() > termino){
+            aux = aux.getSig();
+        }
+        if(aux != null && aux.getInfo().getTermino() == termino){
+            return aux.getInfo().getValor();
+        } else {
+            return 0;
+        }
+    }
+
+    public String mostrar(){
+        Nodo aux = termino_mayor;
+        String pol = "";
+        if(aux != null){
+            while(aux != null){
+                String signo = "";
+                if(aux.getInfo().getValor() >= 0){
+                    signo = "+";
+                }
+                pol += signo + aux.getInfo().getValor() + "x^" + aux.getInfo().getTermino();
+                aux = aux.getSig();
+            }
+        }
+        return pol;
+    }
+
+    public Polinomio sumar(Polinomio polinomio2){
+        Polinomio paux = new Polinomio();
+        Polinomio mayor = this;
+        if(grado < polinomio2.grado){
+            mayor = polinomio2;
+        }
+        for(int i = 0; i< mayor.grado; i++){
+            double total = obtenerValor(i) + polinomio2.obtenerValor(i);
+            if(total != 0){
+                paux.agregarTermino(i, total);
+            }
+        }
+        return paux;
+    }
+
+    public Polinomio multiplicar(Polinomio polinomio2){
+
     }
 }
 
