d2db74f104741c6712053ffc9253bd1d9c06cb10
ana <andelamo@myuax.com>
2023-03-16 13:47:36 +0100

    Implementación de métodos

--- src/main/java/InterfazPolinomio.java
+++ src/main/java/InterfazPolinomio.java
@@ -1,2 +1,12 @@
 public interface InterfazPolinomio {
+
+    public void caragarPolinomio(Polinomio polinomio);
+    public void modificarPolinomio(DatoPolinomio dato, int posicion);
+    public int obtenerValorPolinomio(int x);
+    public Polinomio sumarPolinomios(Polinomio polinomio1, Polinomio polinomio2);
+    public Polinomio multiplicarPolinomios(Polinomio polinomio1, Polinomio polinomio2);
+
+
+
+
 }
--- src/main/java/Polinomio.java
+++ src/main/java/Polinomio.java
@@ -1,12 +1,47 @@
 import java.util.Scanner;
 
-public class Polinomio{
+public class Polinomio implements InterfazPolinomio {
     int grado;
     NodoPolinomio terminoMayor;
+
+    Polinomio polinomioActual;
 
     public Polinomio(int grado, NodoPolinomio terminoMAyor){
         this.grado = grado;
         this.terminoMayor = terminoMayor;
+    }
+
+    @Override
+    public void caragarPolinomio(Polinomio polinomio) {
+        polinomioActual = polinomio;
+
+    }
+
+    @Override
+    public void modificarPolinomio(DatoPolinomio dato, int posicion) {
+        NodoPolinomio nodoActual = polinomioActual.terminoMayor;
+        for(int i = 0; i < posicion; i++){
+            nodoActual = nodoActual.siguiente;
+        }
+        if (nodoActual != null){
+            nodoActual.dato = dato;
+        }
+
+    }
+
+    @Override
+    public int obtenerValorPolinomio(int x) {
+        return 0;
+    }
+
+    @Override
+    public Polinomio sumarPolinomios(Polinomio polinomio1, Polinomio polinomio2) {
+        return null;
+    }
+
+    @Override
+    public Polinomio multiplicarPolinomios(Polinomio polinomio1, Polinomio polinomio2) {
+        return null;
     }
 }
 
